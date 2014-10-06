7b9f8fd4ba1b3719da5464d3e0164f878697e579
thais.marpi@gmail.com <thais.marpi@gmail.com>
2014-10-06 18:09:59 -0300

    Versão atualizada
    copiando o codigo melhorado, as bases dos exercicios, a parte do livro
    que eu ja peguei do site

--- RI 2/Program.cs
+++ RI 2/Program.cs
@@ -6,21 +6,45 @@ namespace RI_2
 {
     class Program
     {
-        public List<string> dicionario = new List<string>();
-
         private static void Main(string[] args)
         {
-            DateTime Antes = DateTime.Now;
-            Console.WriteLine("Começou!");
-            info.contaDocumentos();
-            Dicionario D = new Dicionario();
-            //D.Controle("Vetorial");       //5
-            //D.Controle("GranModerada");   //5
-            D.Controle("GranFina");
-            TimeSpan Time = DateTime.Now - Antes;
-            Console.WriteLine("\nTempo gasto = "+Time.Seconds.ToString());
-            Console.ReadKey();
+            Menu Controla = new Menu();
+            int opc = 0;
+            do
+            {
+                opc = Controla.MenuInicial();
+                if (opc == 1)
+                {
+                    info.contaDocumentos();
+                    Console.WriteLine("\n Encontrados "+info.numDocumentos+" documentos");
+                    Console.ReadKey();
+                }
+                else
+                {
+                    if (podeLer())
+                        break;
+                    else
+                        opc = 1;
+                }
+            } while (opc == 1);           
+            
+            Dicionario D = new Dicionario();            
+            D.Controle();    
         }
+
+        static bool podeLer()
+        {
+            info.contaDocumentos();
+            if (info.numDocumentos == 0)
+            {
+                Console.WriteLine("\n\n Não foi detectado nenhum documento");
+                Console.ReadKey();
+                return false;
+            }
+            return true;
+        }
+
+
     }
-     
+
 }
