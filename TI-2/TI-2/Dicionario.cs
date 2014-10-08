f8ba654f08e5198342c4460bf1a41e525db84cb8
thais.marpi@gmail.com <thais.marpi@gmail.com>
2014-10-08 06:58:27 -0300

    Criado projeto TI-2
    Só copiei algumas funções que se aplicam ao modelo vetoria

---
+++ TI-2/TI-2/Dicionario.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TI_2
+{
+    class Dicionario
+    {
+        public Documento[] Docs = new Documento[info.numDocumentos + 1];
+        public List<Termo> Vocabulario = new List<Termo>();        // Todos os termos
+        private Ordenar O = new Ordenar();
+
+        public void lerDocs()
+        {
+            for (int i = 1; i <= info.numDocumentos; i++)
+            {
+                Docs[i] = new Documento(i, ref Vocabulario);
+            }
+            Ordenar();
+        }
+
+        private void Ordenar()
+        {
+            Termo[] AuxOrdenar = new Termo[Vocabulario.Count];
+            Vocabulario.CopyTo(AuxOrdenar);
+            O.Sort(ref AuxOrdenar, 0, AuxOrdenar.Length - 1);
+            Vocabulario.Clear();
+            Vocabulario.AddRange(AuxOrdenar);
+        }
+
+        private void rotinaModeloVetorial()
+        {
+            FreqInversa();
+            for (int i = 1; i <= info.numDocumentos; i++)
+            {
+                Docs[i].calculaPesos(Vocabulario);
+            }
+        }
+
+        private void FreqInversa()
+        {
+            int Cont;
+            foreach (Termo T in Vocabulario)
+            {
+                Cont = 0;
+                for (int i = 1; i <= info.numDocumentos; i++)
+                {
+                    if (T.frequenciaEmDoc[i] != 0)
+                        Cont++;
+                }
+                T.frequenciaInversa = Math.Log(1 + (info.numDocumentos * 1.0 / Cont), 2);
+            }
+        }
+    }
+}
---
+++ TI-2/TI-2/Termo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TI_2
+{
+    class Termo
+    {
+        /*"Matriz de frequencia" desse termo, quantas vezes ele aparece em cada documento
+         * os documentos vão de 1 a 6, então 7 posições (a zero fica inutilizada)*/
+        public int[] frequenciaEmDoc = new int[info.numDocumentos + 1];
+        public double frequenciaInversa = 0;    // frequencia inversa do termo (total de documento/documentos em que ele aparece)
+        public string palavra;  // a string do termo propriamente dita
+
+        public Termo(string Palavra, int arquivo, int posicao)
+        {
+            /* Para criar um termo basta dizer a palavra e o arquivo onde ela foi inicialmente encontrada e em qual posicao do arquivo ela esta*/
+            palavra = Palavra;
+            frequenciaEmDoc[arquivo] = 1;           
+        }
+
+        /// <summary>
+        /// Usa a formula dada e calcula a frequencia inversa do termo
+        /// </summary>
+        private void FreqInversa()
+        {
+            int Cont;
+            Cont = 0;
+            for (int i = 1; i <= info.numDocumentos; i++)
+            {   // Conta em quantos documentos o termo aparece
+                if (frequenciaEmDoc[i] != 0)
+                    Cont++;
+            }
+            frequenciaInversa = Math.Log(1 + (info.numDocumentos * 1.0 / Cont), 2);
+            // A frequencia inversa é dada por essa formula log2(1 + total de doc/doc em que o termo aparece)
+        }
+    }
+}
